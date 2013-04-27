80a3cb23bfdd08a44cf840d3c915bfa8ad2432bc
Ye Joo Park <subwaymatch@gmail.com>
2013-04-27 08:54:11 -0500

    First upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+	require('typeprotect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Basic Page Needs
+	================================================== -->
+	<meta charset="utf-8">
+	<title>Example Password-protected Page</title>
+
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.8.0/build/cssreset/cssreset-min.css">
+	<style>
+		
+	html, body {
+		height: 100%;
+	}
+	
+	body {
+		font: normal 14px/24px Helvetica, Arial, sans-serif;
+		background: #f5f5f5;
+		padding-top: 80px; 
+	}
+
+	#box {
+		width: 300px;
+		background: #fff;
+		padding: 30px 50px 50px 50px;
+		border: 1px solid #eee; 
+		margin: 0 auto;
+	}
+	
+	h1 {
+		color: #22aa33;
+		font: bold 18px/28px Helvetica, Arial, sans-serif;
+		padding-bottom: 15px;
+		border-bottom: 1px solid #eee;
+		margin-bottom: 10px; 
+	}
+
+	p {
+		color: #444;
+		margin: 20px 0;
+	}
+
+	code {
+		font: normal 15px/25px "Courier New", monospace;
+		color: #22aa33;
+	}
+
+	a.button {
+		color: #fff; 
+		background: #22aa33; 
+		font: bold 15px/25px Helvetica, Arial, sans-serif;
+		text-transform: uppercase;
+		letter-spacing: 2px; 
+		text-align: center; 
+		margin-top: 40px; 
+		display: block; 
+		width: 100%; 
+		padding: 25px 0; 
+		border: none; 
+		text-decoration: none;
+							
+		-moz-border-radius: 5px;
+		-webkit-border-radius: 5px;
+		border-radius: 5px; /* future proofing */
+		-khtml-border-radius: 5px; /* for old Konqueror browsers */
+	}
+	
+		a.button:hover {
+			color: #22aa33; 
+			background: #000; 
+		}
+	
+	</style>
+
+</head>
+<body>
+	
+	<div id="box">
+		<h1>Success!</h1>
+
+		<p>You can only view this page after entering the correct password</p>
+
+		<p>Want to create a sign-out link? It's as easy as copying and pasting the code below</p>
+
+		<code>
+			&lt;a href="typeprotect.php?signout=1"&gt;Sign Out&lt;/a&gt;
+		</code>
+
+		<a href="typeprotect.php?signout=1" class="button">Sign Out</a>
+	</div><!-- // #box -->
+
+</body>
+</html>
---
+++ typeprotect.php
@@ -0,0 +1,168 @@
+<?php
+
+	// sha1() encrypted password
+	// the default is "test"
+	$password = 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3';
+	
+	// Start session
+	session_start();
+
+	// Initialize wrong password check variable
+	$wrongPass = false;
+	
+	if( !isset( $_SESSION['signedIn'] ) ) {
+		$_SESSION['signedIn'] = false;
+	}
+	
+	// If the user clicked "sign out", 
+	if( isset( $_GET['signout'] ) ) {
+		$_SESSION['signedIn'] = false;
+
+		// Change the location to where you want to redirect the user after signing out
+		header( "Location: index.php" );
+	}
+	
+	// If the user submitted a password
+	if( isset( $_POST['password'] ) ) {
+		if ( sha1( $_POST['password'] ) == $password ) {
+			$_SESSION['signedIn'] = true;
+		} else {
+			$wrongPass = true;
+		}
+	}
+	
+	if( !$_SESSION['signedIn']):
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Password Protected Page</title>
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<meta name="robots" content="noindex, nofollow">
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.8.0/build/cssreset/cssreset-min.css">
+		<style>
+			
+			html, body {
+				height: 100%;
+			}
+			
+			body {
+				font: normal 16px/26px Helvetica, Arial, sans-serif;
+				background: #f5f5f5;
+				padding-top: 80px; 
+			}
+			
+			h1 {
+				color: #ff5544;
+				font: bold 18px/28px Helvetica, Arial, sans-serif;
+				padding-bottom: 15px;
+				border-bottom: 1px solid #eee;
+				margin-bottom: 10px; 
+			}
+			
+			#box-signIn {
+				width: 300px;
+				background: #fff;
+				padding: 30px 50px 50px 50px;
+				border: 1px solid #eee; 
+				margin: 0 auto;
+			}
+			
+			div.error {
+				color: #ff5544;
+				font: normal 14px/24px Helvetica, Arial, sans-serif;
+				margin-top: 20px; 
+			}
+			
+			@media only screen and (max-width: 479px) {
+				#box-signIn {
+					width: 240px; 
+					padding: 20px 30px 30px 30px;
+				}
+			}
+			
+			form#signIn {
+				
+			}
+			
+				form#signIn label {
+					color: #ddd; 
+					font: normal 12px/22px Helvetica, Arial, sans-serif;
+					text-transform: uppercase;
+					letter-spacing: 2px; 
+					display: block;
+					margin: 25px 0 15px 0; 
+				}
+				
+				form#signIn input[type="password"],
+				form#signIn input[type="text"] {
+					color: #777;
+					border: 1px solid #eee;
+					padding: 20px 9%; 
+					width: 80%; 
+					outline: none; 
+					display: block; 
+				}
+				
+					form#signIn input[type="password"]:focus,
+					form#signIn input[type="text"]:focus {
+						border: 1px solid #ff5544; 
+					}
+			
+				form#signIn input.submit {
+					color: #fff; 
+					background: #ff5544; 
+					font: bold 15px/25px Helvetica, Arial, sans-serif;
+					text-transform: uppercase;
+					letter-spacing: 2px; 
+					margin-top: 40px; 
+					width: 100%; 
+					padding: 25px 0; 
+					border: none; 
+					cursor: pointer; 
+										
+					-moz-border-radius: 5px;
+					-webkit-border-radius: 5px;
+					border-radius: 5px; /* future proofing */
+					-khtml-border-radius: 5px; /* for old Konqueror browsers */
+				}
+				
+					form#signIn input.submit:hover {
+						color: #ff5544; 
+						background: #000; 
+					}
+					
+		</style>
+	</head>
+	
+	<body>
+		<div id="box-signIn">
+			<h1>Protected Page</h1>
+			
+			<?php if( $wrongPass ) { ?>
+				<div class="error">Wrong password<br />Default password is "test".</div>
+			<?php } ?>
+			
+			<form id="signIn" method="post">
+				<label for="username" style="display: none; ">Username</label>
+				<input type="text" id="username" name="username" style="display: none; " />
+				
+				<label for="password">Password</label>
+				<input type="password" id="password" name="password" />
+				<input type="submit" name="submit" class="submit" value="Sign In" />
+			</form>
+		</div>
+	</body>
+</html>
+
+<?php 
+	// Exit
+	exit();
+	endif;
+?>
